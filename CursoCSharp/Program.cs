57b1edf0c0cdeb6af3e9814ac7eb2f6fdc9a3336
migue <migue@DESKTOP-FKG1T2H>
2022-10-19 20:27:35 -0300

    Adicionar arquivos de projeto.

---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
